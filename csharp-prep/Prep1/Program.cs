44517fbeb1821f41e89333518290caa95f5029ac
Zach Holcomb <holcombzv@byui.edu>
2023-09-16 14:24:24 -0600

    Prep 1 and 2 completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Prompt user for first name
+        Console.WriteLine("What is your first name?");
+        string fname = Console.ReadLine();
+
+        //Prompt user for last name
+        Console.WriteLine("What is your last name? ");
+        string lname = Console.ReadLine();
+
+        //Display name in James Bond style
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //Prompt user for score
+        Console.WriteLine("What is your score?");
+        int score = int.Parse(Console.ReadLine());
+
+        //If statements determine which letter grade to assign and whether the user passed or failed
+        string letter = "";
+        bool passed = false;
+
+        if (score >= 90)
+        {
+            letter = "A";
+            passed = true;
+        }
+
+        else if (score >= 80)
+        {
+            letter = "B";
+            passed = true;
+        }
+
+        else if (score >= 70)
+        {
+            letter = "C";
+            passed = true;
+        }
+
+        else if (score >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        //Display letter grade earned
+        Console.WriteLine($"Your earned a {letter}.");
+        if (passed)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("You didn't pass. Better luck next time.");
+        }
+
     }
 }
