0c9e5695561fe49120fc37ef626dfa74ef1db5fc
Zach Holcomb <holcombzv@byui.edu>
2023-09-18 09:45:40 -0600

    Finished assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Prompt user for "magic number"
+        Console.WriteLine("What is the magic number?");
+        int magNum = int.Parse(Console.ReadLine());
+
+        //Prompt the user for guesses until they guess correctly
+        bool correct = false;
+        while (!correct)
+        {
+            Console.WriteLine("\nWhat is your guess?");
+            int guess = int.Parse(Console.ReadLine());
+
+            if (guess > magNum)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (guess < magNum)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else
+            {
+                correct = true;
+            }
+        }
+
+        Console.WriteLine("Congratulations! You guessed right!");
     }
 }
