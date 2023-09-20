9fc95b32d85853f2b5a01246fd4008a0b1f9d04e
Zach Holcomb <holcombzv@byui.edu>
2023-09-20 09:48:08 -0600

    Completed prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -8,14 +8,25 @@ class Program
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
 
         List <int> numbers = new List<int>();
+        int newNum = 0;
         do
         {
             Console.Write("Enter whole number: ");
-            int newNum = int.Parse(Console.ReadLine());
+            newNum = int.Parse(Console.ReadLine());
             if (newNum != 0)
             {
                 numbers.Add(newNum);
             }
         } while (newNum != 0);
+
+        //Calculate and display the sum, average, and max values
+        int sum = numbers.Sum();
+        Console.WriteLine($"The sum is: {sum}");
+
+        double mean = numbers.Average();
+        Console.WriteLine($"The average is: {mean}");
+
+        int max = numbers.Max();
+        Console.WriteLine($"The max value is: {max}");
     }
 }
