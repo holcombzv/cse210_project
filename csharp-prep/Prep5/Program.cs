50d976bf7debd6761f3d6f48008a2577c78f8eed
Zach Holcomb <holcombzv@byui.edu>
2023-09-20 12:02:56 -0600

    Completed prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayWelcome()
+        {
+            //Displays a welcome message for the user
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string PromptUserName()
+        {
+            //Prompts the user to enter their name
+            string name;
+            Console.WriteLine("\nPlease enter your name: ");
+            name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            //Prompt the user for a number
+            Console.WriteLine("\nPlease enter your favorite number: ");
+            int num = int.Parse(Console.ReadLine());
+            return num;
+        }
+
+        static int SquareNumber(int num)
+        {
+            //Squares the number "num" and returns it.
+            int squared = num * num;
+            return squared;
+        }
+
+        static void DisplayResult(string name, int num)
+        {
+            //Displays results
+            Console.WriteLine($"\n{name}, your number squared is: {num}");
+        }
+
+        DisplayWelcome();
+        string  name = PromptUserName();
+        int userNum = PromptUserNumber();
+        int newNum = SquareNumber(userNum);
+        DisplayResult(name, newNum);
     }
 }
