4d5a303f76499f65568dc2f76fbe37f9764b663e
Zach Holcomb <holcombzv@byui.edu>
2023-12-04 09:11:47 -0700

    Finished FIveCrown class.

--- final/FinalProject/FiveCrown.cs
+++ final/FinalProject/FiveCrown.cs
@@ -7,7 +7,18 @@ public class FiveCrown : Game
     }
     public override void End()
     {
-        throw new NotImplementedException();
+        Console.WriteLine($"Game Over!!!");
+        List<User> sortedPlayers = base.getPlayers().OrderBy(player => player.GetPoints()).ToList();
+        int n = 1;
+        foreach(User player in sortedPlayers)
+        {
+            Console.WriteLine($"{n}. {player.GetName()} {player.GetPoints()}");
+            n ++;
+        }
+        Console.WriteLine("\nPress Enter to end");
+        Console.ReadLine();
+        Console.Clear();
+        System.Environment.Exit(0);
     }
     public override void Score()
     {
@@ -21,12 +32,13 @@ public class FiveCrown : Game
     public override void Start()
     {
         base.Start();
+        _round = 1;
         while(true)
         {
             if(_round <= 11)
             {
                 Score();
-                Console.WriteLine($"Current totals:");
+                Console.WriteLine($"Round {_round} totals:");
                 int n = 1;
                 foreach(User player in base.getPlayers())
                 {
